942153d76c886ee48f87fa59ca62c6cf1cafef71
Albert Bori <abori@rushline.com>
2015-03-05 10:07:05 -0700

    Added more attractive loading sequence (to prevent users from seeing the cell resizing happening before their eyes)

--- MultipleWebViews/ResizeAfterTableViewController.swift
+++ MultipleWebViews/ResizeAfterTableViewController.swift
@@ -8,22 +8,35 @@
 
 import Foundation
 import UIKit
+import MBProgressHUD
 
 class ResizeAfterTableViewController: UITableViewController {
     
     var data: [ResizeAfterCellData] = []
+    var loadingCover: UIView!
     
     
     //MARK: - UIViewController
     
     override func viewDidLoad() {
         tableView.registerNib(UINib(nibName: "ResizeAfterCell", bundle: NSBundle.mainBundle()), forCellReuseIdentifier: "ResizeAfterCell")
+        loadingCover = UIView(frame: tableView.frame)
+        loadingCover.backgroundColor = tableView.backgroundColor
+        self.view.addSubview(loadingCover)
         
         var content = Model.getTestData()
         for i in 0..<content.count {
             var contentData = ResizeAfterCellData()
             contentData.content = content[i]
             data.append(contentData)
+        }
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+    }
+    
+    override func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if loadingCover.superview != nil && data.filter({ cellData in cellData.height != nil }).count >= tableView.visibleCells().count {
+            loadingCover.removeFromSuperview()
+            MBProgressHUD.hideHUDForView(self.view, animated: true)
         }
     }
     
